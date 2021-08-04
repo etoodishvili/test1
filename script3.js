60a91c481c37a8836708be430b8205ae81d5e8b0
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-08-05 00:10:03 +0400

    add responsive

--- script3.js
+++ script3.js
@@ -62,22 +62,32 @@ $('.starRating span').click(function(){
 
 
 
-// dots
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length} ;
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 
 
 
+
+
+
+// likes
+const likes = document.querySelectorAll(".fa-thumbs-up");
+likes.forEach(likeIcon => {
+  likeIcon.addEventListener("click", () => {
+    console.log(likeIcon)
+    if(likeIcon.style.color == "green") {
+      likeIcon.style.color = "#C1C1C1";
+    } else {
+      likeIcon.style.color = "green"
+    }
+  })
+});
+const disLikes = document.querySelectorAll(".fa-thumbs-down");
+disLikes.forEach(likeIcon => {
+  likeIcon.addEventListener("click", () => {
+    console.log(likeIcon)
+    if(likeIcon.style.color == "red") {
+      likeIcon.style.color = "#C1C1C1";
+    } else {
+      likeIcon.style.color = "red"
+    }
+  })
+});
