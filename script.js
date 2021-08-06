7a1b37deb4687ad26eb9d1a819d504cd54aeeb0a
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-08-06 11:01:18 +0400

    add

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 let togglebutton = document.getElementById('togglebutton');
 let navbarlinks = document.getElementById('navbarlinks');
-
 togglebutton.addEventListener('click',function() {
   navbarlinks.classList.toggle('active');
 })
-
-let box = document.getElementById('box');
-let navbarlinks = document.getElementById('navbarlinks');
-
-togglebutton.addEventListener('click',function() {
-  navbarlinks.classList.toggle('active');
-})
-
-
 const productPreview = document.querySelector('.product_preview');
 const bottomSide = document.querySelector('.bottom-side');
 const images = document.querySelectorAll('.little1');
@@ -21,7 +11,6 @@ const topSide = document.createElement('div');
 productPreview.appendChild(topSide);
 topSide.classList.add('top-side');
 topSide.style.cssText+= `background-image: url('images/paraiso1.jpg')`;
-
 images.forEach(image => {
     image.addEventListener('click', function() {
         let src = image.src;
@@ -29,39 +18,21 @@ images.forEach(image => {
         
     })
 })
-
-
 const sizes = document.querySelectorAll('.size');
-
 function changeSize(){
   sizes.forEach(size=> size.classList.remove('active'));
   this.classList.add('active');
 }
-
 sizes.forEach(size => size.addEventListener('click',changeSize));
-
-
-
-
-
-
-
-
-
 // star
-
-$('.starRating span').click(function(){
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-  $(this).parent().addClass('starRated');
-  
-  // Added for Demo
-  let rating = $(this).index() + 1;
-  $('#currentRating').html( "<small>Rating: <b>" + rating + "</b></small>" );
+const starDating = document.querySelector('.starRating');
+const spans = starDating.querySelectorAll('span');
+spans.forEach(element => {
+  element.addEventListener('click', () => {
+    element.classList.add('active');
+    starDating.classList.add('starRated');
+  })
 });
-
-
-
 // dots
 function showSlides(n) {
   var i;
@@ -78,6 +49,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-
-
+const likes = document.querySelectorAll(".fa-thumbs-up");
+likes.forEach(likeIcon => {
+  likeIcon.addEventListener("click", () => {
+    if(likeIcon.style.color == "green") {
+      likeIcon.style.color = "#C1C1C1";
+    } else {
+      likeIcon.style.color = "green"
+    }
+  })
+});
+const disLikes = document.querySelectorAll(".fa-thumbs-down");
+disLikes.forEach(likeIcon => {
+  likeIcon.addEventListener("click", () => {
+    if(likeIcon.style.color == "red") {
+      likeIcon.style.color = "#C1C1C1";
+    } else {
+      likeIcon.style.color = "red"
+    }
+  })
+});
