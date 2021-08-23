48af47eda726f0c71512bce7e9d1e4efbda84bc6
etoodishvili <82343074+etoodishvili@users.noreply.github.com>
2021-08-24 02:00:58 +0400

    add

--- script.js
+++ script.js
@@ -70,19 +70,39 @@ disLikes.forEach(likeIcon => {
   })
 });
 // counter
-$(document).ready(function() {
-  $('.minus').click(function () {
-    var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
-    $input.val(count);
-    $input.change();
-    return false;
-  });
-  $('.plus').click(function () {
-    var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1);
-    $input.change();
-    return false;
-  });
-});
+const input = document.querySelector('input')
+const minBtn = document.querySelector('#min')
+const plusBtn = document.querySelector('#plus')
+const minValue = 0
+const maxValue = 12
+
+function checkButton() {
+  if (input.value == minValue) {
+    minBtn.disabled = true
+    plusBtn.disabled = false
+  } else if (input.value == maxValue) {
+    minBtn.disabled = false
+    plusBtn.disabled = true
+  } else {
+    minBtn.disabled = false
+    plusBtn.disabled = false
+  }
+}
+
+// Set button when first loaded
+window.addEventListener('DOMContentLoaded', checkButton)
+
+// Set button when input was changed
+input.addEventListener('input', checkButton)
+
+
+// Add listener when button has clicked
+minBtn.addEventListener('click', () => {
+  input.value = Number(input.value) - 1
+  checkButton()
+})
+
+plusBtn.addEventListener('click', () => {
+  input.value = Number(input.value) + 1
+  checkButton()
+})
